132613dbb595e81f0baf241f7fa70638599ed73c
Tuhin Chakraborty <chak_tuhin@yahoo.in>
2014-09-13 19:21:34 +0530

    changed the project
    hello world

--- main.c
+++ main.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <string.h>
-
-int main(int argc, char **argv)
+int main()
 {
-	char first[255], last[255];
-
-	printf("Enter your first name: ");
-	fgets(first, 255, stdin);
-	first[strlen(first)-1] = '\0'; /* remove the newline at the end */
-
-
-	gets(last); /* buffer overflow? what's that? */
-
-	printf("Hello %s %s!\n", first, last);
- 	return 0;
+	printf("Hello world");
 }
